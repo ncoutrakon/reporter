dcfdadc952fdad6e0f50178bc5cf10217e63b1d9
ncoutrakon <ncoutrakon@gmail.com>
2017-05-01 13:06:11 -0500

    ymin for cum plots, title, and overlay cum plots legend

--- daily_report.R
+++ daily_report.R
@@ -1,33 +1,19 @@
 library(ggplot2); library(reshape2); library(xts); library("scales")
 source("/users/nick/documents/daytrader/funs.R")
 setwd("/users/nick/documents/daytrader/reporter")
-file_log <- file("daily.txt", "a")
+
 options(stringsAsFactors = F)
 
 filename <- "/SierraChart/TradeActivityLogs/SIM_TradesList.txt"
-get_stats <- function(trade_df){
-        trade_date <- as.character(as.Date(trade_df$Entry.DateTime[1]))
-        num_trades <- length(unique(trade_df$Entry.DateTime))
-        winners <- length(unique(trade_df$Entry.DateTime[trade_df$Profitable]))
-        pnl <- round(sum(trade_df$Profit.Loss) / num_trades, 2)
-        runup <- round(sum(trade_df$Runup) / num_trades, 2)
-        drawdown <- round(sum(trade_df$Drawdown) / num_trades, 2)
-        win_rate <- round(100*winners / num_trades, 4)
-        profitable <- pnl > 0
-        trade_stats <- data.frame(Date = trade_date, Winners = winners, Trades = num_trades,
-                                  Profit.Loss = pnl, Runup = runup, Drawdown = drawdown,
-                                  Win.Rate = win_rate, Total.PnL = pnl*num_trades, Profitable = profitable)
-        return(trade_stats)
-}
 
 
 ##################################### MUNGE #############################################
 # Reads and formats trades file
 trades <- read.table(filename, sep = "\t", header = T)
-writeLines(paste(Sys.time(), "Trade file read..."), file_log)
+
 trades <- trades[, c(1:10, 13:15)]
 names(trades)[c(9, 10, 12, 13)] <- c("Runup", "Drawdown", "Profit.Loss", "Cum.P.L")
-trades$Profitable <- (trades$Profit.Loss > 0)
+trades$Profitable <- (trades$Profit.Loss >= 0)
 trades$Index <- row.names(trades)
 
 
@@ -62,7 +48,7 @@ cumpnl_df <- data.frame(index(cumpnl_df),  cumpnl_df[,1])
 names(cumpnl_df) <- c("Timestamp", "Cum.Profit.Loss")
 cumpnl_df$Exit <- c(0, (diff(cumpnl_df$Cum.Profit.Loss) != 0)*1)
 
-writeLines(paste(Sys.time(), "Trade dataframe munged..."), file_log)
+
 
 ################################ PLOTS #########################################
 # Plots daily PnL histogram and title with  which_day EOD stats
@@ -70,8 +56,9 @@ daily_stat <- day_stats[dim(day_stats)[1], 2:7]
 daily_pl_title <- paste(names(daily_stat), daily_stat, sep = ': ',collapse = "   ")
 day_pnl_pl <- ggplot(day_stats) + geom_bar(aes(x = Date, y = Total.PnL), stat = "identity")  +
         scale_y_continuous(minor_breaks = seq(-500, 500, 5), breaks = seq(-500, 500, 10)) +
-        theme(axis.title.x = element_blank()) +  ggtitle(daily_pl_title) +
-        geom_hline(yintercept = 0, color = "red") + scale_x_date(date_breaks = "1 day", date_minor_breaks = "1 day", date_labels = "%m-%d")
+        theme(axis.title.x = element_blank(), plot.title = element_text(size = 10)) +  ggtitle(daily_pl_title) +
+        geom_hline(yintercept = 0, color = "red") +
+        scale_x_date(date_breaks = "1 day", date_minor_breaks = "1 day", date_labels = "%m-%d")
 
 # Plots average daily Runup, Drawdown, and PnL per trade for each day
 day_stats$Index <- factor(index(day_stats), levels = index(day_stats))
@@ -94,7 +81,7 @@ cumpnl_pl <- ggplot(cumpnl_df, aes(x = Timestamp, y = Cum.Profit.Loss)) +
         geom_hline(yintercept = 0, color = "red") +
         geom_label(data = cumpnl_df[cumpnl_df$Exit !=0, ],
                    aes(x = Timestamp, y = Cum.Profit.Loss, label = format(Timestamp, "%H:%M:%S")),
-                   fill = "sky blue", color = "white", size = 2) +
+                   fill = "sky blue", color = "white", size = 2) + expand_limits(y=-10) + expand_limits(y=10) +
         scale_x_datetime(breaks = date_breaks("1 hour"), labels = date_format("%Hh", tz = "America/Chicago"))
 
 
@@ -116,16 +103,12 @@ trade_stats_pl <- ggplot(trade_stats_df) +
         geom_vline(xintercept = day_delim) + geom_hline(yintercept = 0, color = "red")
 
 
-writeLines(paste(Sys.time(), "Plots created..."), file_log)
+
 ############################ OUTPUT ##########################################
 pdf(paste0("reports/", trade_date, ".pdf"), width = 8.5, height = 11)
 multiplot(day_pnl_pl, day_stats_pl, cumpnl_pl, trade_stats_pl, cols = 1)
 dev.off()
 
-writeLines(paste(Sys.time(), "Plots saved..."), file_log)
-
 # Opens the PDF for viewing
 system2('open', args = paste0("reports/", trade_date, ".pdf"), wait = F)
 
-writeLines(paste(Sys.time(), "Plots displayed...\n"), file_log)
-close(file_log)
--- weekly.R
+++ weekly.R
@@ -39,20 +39,20 @@ trades <- trades[order(trades$Exit.DateTime),]
 # Convert Total.Efficiency columns to decimals
 trades[, 11] <- as.numeric(substr(trades[, 13], 1, nchar(trades[, 13])-1))/100
 trades$Profitable <- (trades$Profit.Loss > 0)
-trades$Index <- row.names(trades)
+trades$Index <- factor(row.names(trades))
 last_date <- as.Date(trades[dim(trades)[1], 'Exit.DateTime'])
-last_week_date <- last_date - as.numeric(format(last_date, "%w")) - 7
-
+last_week_date <- last_date - as.numeric(format(last_date, "%w"))
+trades <- trades[as.Date(trades$Exit.DateTime) > last_week_date, ]
 
 
 
 ######################## PLOTS ###############################
 # Overlapped Daily CumPnL
+daily_stat <- get_stats(trades)[1, 2:8]
+pl_title <- paste(names(daily_stat), daily_stat, sep = ': ',collapse = "   ")
 
 trades_cum <- as.xts(trades[, c(4:7, 9:13)], order.by = trades$Exit.DateTime)
-trades_cum <- trades_cum[paste0(last_week_date, "/"), ]
 by_day_cum <- suppressWarnings(split(trades_cum, as.Date(index(trades_cum))))
-
 
 trades_cum <- do.call(cbind, lapply(by_day_cum, to_daily_cum))
 trades_cum <- data.frame(Timestamp = index(trades_cum), trades_cum)
@@ -61,25 +61,26 @@ trades_cum <- melt(trades_cum, id.vars = "Timestamp")
 names(trades_cum) <- c("Timestamp", "Date", "Ticks")
 cum_pl <- ggplot(trades_cum) + geom_line(aes(x = Timestamp, y = Ticks, color = Date)) + 
         scale_x_datetime(breaks = date_breaks("1 hour"), labels = date_format("%Hh", tz = "America/Chicago")) +
-        xlab("") + scale_y_continuous(breaks = seq(-100, 100, 20)) + geom_hline(yintercept = 0, color = "red")
+        xlab("") + scale_y_continuous(breaks = seq(-100, 100, 10)) + geom_hline(yintercept = 0, color = "red") +
+        ggtitle(pl_title) + theme(legend.position = c(.1, .9), plot.title = element_text(size = 10)) +
+        expand_limits(y=-10) + expand_limits(y=10)
 
 
 
 # total runups
-trade_stats <- trades[as.Date(trades$Exit.DateTime) > last_week_date, ]
-day_delim <- (diff(as.Date(trade_stats$Entry.DateTime)) != 0)
+day_delim <- (diff(as.Date(trades$Entry.DateTime)) != 0)
 day_delim <- day_delim*index(day_delim) +.5
 day_delim <- sort(unique(day_delim))[-1]
 
-trade_stats$Index <- factor(trade_stats$Index, levels = trade_stats$Index)
-stats_pl <- ggplot(trade_stats) +
+
+stats_pl <- ggplot(trades) +
         geom_bar(aes(x = Index, y =Profit.Loss, fill = Profitable), stat = "identity") +
         geom_bar(aes(x = Index, y = Drawdown, fill = Profitable), width = .1, stat = "identity") +
         geom_bar(aes(x = Index, y = Runup, fill = Profitable), width = .1, stat = "identity") +
         theme(legend.position="none", axis.title.x = element_blank(),
               axis.ticks.x = element_blank(), axis.text.x = element_blank()) +
         scale_y_continuous(minor_breaks = seq(-100 , 100, 2.5), breaks = seq(-20, 50, 5)) + 
-         geom_hline(yintercept = 0) + geom_vline(xintercept = day_delim) + ylab("Ticks")
+         geom_hline(yintercept = 0) + geom_vline(xintercept = day_delim) + ylab("")
 
 
 
@@ -87,8 +88,8 @@ stats_pl <- ggplot(trade_stats) +
 
 
 ############################ OUTPUT ##########################################
-out_name <- paste0("reports/", last_date, "_W.pdf") 
-pdf(out_name, width = 11, height = 8.5)
+out_name <- paste0("reports/", last_week_date, "_W.pdf") 
+pdf(out_name, width = 8.5, height = 11)
 multiplot(cum_pl, stats_pl, cols = 1)
 dev.off()
 
